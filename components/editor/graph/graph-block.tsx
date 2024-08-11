a85163672c3422e684ce9518de426689c288a5a2
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-08-11 14:56:59 +0800

    fix: graph losing focus

--- components/editor/graph/graph-block.tsx
+++ components/editor/graph/graph-block.tsx
@@ -2,7 +2,7 @@ import "desmos";
 
 import { InlineContentSchema, StyleSchema } from "@blocknote/core";
 import { ReactCustomBlockRenderProps } from "@blocknote/react";
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 
 import { GraphBlockConfig } from ".";
 
@@ -14,7 +14,19 @@ export const GraphBlock: FC<
   >
 > = ({ block, editor, contentRef }) => {
   const graphRef = useRef<HTMLDivElement>(null);
+  const [graphState, setGraphState] = useState(block.props.state);
+  const [lastUpdated, setLastUpdated] = useState(Date.now());
 
+  const updateEditor = () => {
+    editor.updateBlock(block.id, {
+      props: {
+        ...block.props,
+        state: JSON.stringify(graphState),
+      },
+    });
+  };
+
+  // can only initialize GraphingCalculator after graphRef is mounted
   useEffect(() => {
     if (!graphRef.current) return;
 
@@ -24,16 +36,21 @@ export const GraphBlock: FC<
       calculator.setState(JSON.parse(block.props.state));
 
     calculator.observeEvent("change", () => {
-      editor.updateBlock(block.id, {
-        props: {
-          ...block.props,
-          state: JSON.stringify(calculator.getState()),
-        },
-      });
+      setGraphState(calculator.getState());
     });
 
+    // on subsequent callbacks, a duplicate graph is appended to the graphRef
+    // this is a workaround to remove the duplicate graphs
     graphRef.current.replaceChildren(graphRef.current.children[0]);
   }, [editor, block.id, block.props]);
 
-  return <div ref={graphRef} className="w-full h-96"></div>;
+  return (
+    <div
+      ref={graphRef}
+      // update when Desmos input loses focus,
+      // otherwise interactive element will lose focus while user is still typing
+      onBlur={updateEditor}
+      className="w-full h-96"
+    ></div>
+  );
 };
