5afb358b1e8bc82af279be4113a47b4867fc19ef
TheMythologist <leekaixuan2001@gmail.com>
2024-08-28 21:14:19 +0900

    refactor: memoize `Editor` component

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -2,7 +2,6 @@
 
 import { useMutation, useQuery } from "convex/react";
 import dynamic from "next/dynamic";
-import { useMemo } from "react";
 
 import { Cover } from "@/components/cover";
 import { Toolbar } from "@/components/toolbar";
@@ -17,10 +16,7 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
-  const Editor = useMemo(
-    () => dynamic(() => import("@/components/editor"), { ssr: false }),
-    [],
-  );
+  const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
