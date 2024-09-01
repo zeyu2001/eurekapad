c7840615b50c97dfab4bee0fcc0910b82d4d2385
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-09-01 16:09:14 +0800

    refactor: load desmos beforeInteractive

--- components/editor/graph/graph-block.tsx
+++ components/editor/graph/graph-block.tsx
@@ -2,10 +2,7 @@ import "desmos";
 
 import { InlineContentSchema, StyleSchema } from "@blocknote/core";
 import { ReactCustomBlockRenderProps } from "@blocknote/react";
-import Script from "next/script";
 import { FC, useEffect, useRef, useState } from "react";
-
-import { Spinner } from "@/components/spinner";
 
 import { GraphBlockConfig } from ".";
 import { graphStateJSONSchema } from "./schemas";
@@ -21,7 +18,6 @@ export const GraphBlock: FC<
   const result = graphStateJSONSchema.safeParse(block.props.state);
 
   const graphRef = useRef<HTMLDivElement>(null);
-  const [ready, setReady] = useState<boolean>(false);
   const [graphState, setGraphState] = useState<GraphState | null>(
     result.error ? null : result.data,
   );
@@ -37,7 +33,7 @@ export const GraphBlock: FC<
 
   // can only initialize GraphingCalculator after graphRef is mounted
   useEffect(() => {
-    if (!ready || !graphRef.current) return;
+    if (!graphRef.current) return;
 
     const calculator = Desmos.GraphingCalculator(graphRef.current);
 
@@ -51,24 +47,15 @@ export const GraphBlock: FC<
     // this is a workaround to remove the duplicate graphs
     while (graphRef.current.children.length > 1)
       graphRef.current.removeChild(graphRef.current.lastChild as ChildNode);
-  }, [ready, graphRef, graphState]);
+  }, [graphRef, graphState]);
 
   return (
-    <>
-      <Script
-        src="https://www.desmos.com/api/v1.9/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"
-        onLoad={() => setReady(true)}
-        onReady={() => setReady(true)}
-      />
-      <div
-        ref={graphRef}
-        // update when Desmos input loses focus,
-        // otherwise interactive element will lose focus while user is still typing
-        onBlur={updateEditor}
-        className="w-full h-96 flex items-center justify-center"
-      >
-        {!ready && <Spinner />}
-      </div>
-    </>
+    <div
+      ref={graphRef}
+      // update when Desmos input loses focus,
+      // otherwise interactive element will lose focus while user is still typing
+      onBlur={updateEditor}
+      className="w-full h-96 flex items-center justify-center"
+    ></div>
   );
 };
