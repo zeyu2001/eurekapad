4288f7c43af351a87a71911b3096fd64a3d17b60
TheMythologist <leekaixuan2001@gmail.com>
2024-09-04 01:04:16 +0900

    refactor: use `emoji-mart` instead

--- components/icon-picker.tsx
+++ components/icon-picker.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import EmojiPicker, { EmojiStyle, Theme } from "emoji-picker-react";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
 import { useTheme } from "next-themes";
 
 import {
@@ -21,25 +22,16 @@ export const IconPicker = ({
   asChild,
 }: IconPickerProps) => {
   const { resolvedTheme } = useTheme();
-  const currentTheme = (resolvedTheme || "light") as keyof typeof themeMap;
-
-  const themeMap = {
-    dark: Theme.DARK,
-    light: Theme.LIGHT,
-  };
-
-  const theme = themeMap[currentTheme];
+  const currentTheme = resolvedTheme || "light";
 
   return (
     <Popover>
       <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
       <PopoverContent className="p-0 w-full border-none shadow-none">
-        <EmojiPicker
-          height={350}
-          theme={theme}
-          onEmojiClick={(data) => onChange(data.emoji)}
-          lazyLoadEmojis
-          emojiStyle={EmojiStyle.NATIVE}
+        <Picker
+          data={data}
+          theme={currentTheme}
+          onEmojiSelect={(data) => onChange(data.native)}
         />
       </PopoverContent>
     </Popover>
