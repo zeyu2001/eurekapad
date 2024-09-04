5b8ddf97dc26c3166e64fce9a9920e692b6cc170
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-09-04 20:30:14 +0800

    fix: emoji-mart types

--- components/icon-picker.tsx
+++ components/icon-picker.tsx
@@ -2,13 +2,8 @@
 
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
+import type { Emoji as EmojiMart } from "emoji-mart";
 import { useTheme } from "next-themes";
-
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 
 interface IconPickerProps {
   onChange: (_icon: string) => void;
@@ -16,20 +11,61 @@ interface IconPickerProps {
   asChild?: boolean;
 }
 
+/**
+ * Currently, type definitions in `@emoji-mart/react` isn't perfect, so we implement our own.
+ * ref. https://github.com/missive/emoji-mart/issues/576
+ */
+
+type Emoji = typeof EmojiMart.Props;
+
+interface Category {
+  id: string;
+  name: string;
+  emojis: string[];
+}
+
+interface Data {
+  compressed: boolean;
+  categories: Category[];
+  emojis: Record<string, Emoji>;
+  aliases: Record<string, string>;
+}
+
+interface PickerProps {
+  data: Data;
+  theme: "auto" | "light" | "dark";
+  onEmojiSelect: (_emoji: Emoji) => void;
+}
+
+const EmojiPicker = (props: PickerProps) => {
+  return <Picker {...props} />;
+};
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 export const IconPicker = ({
   onChange,
   children,
   asChild,
 }: IconPickerProps) => {
   const { resolvedTheme } = useTheme();
-  const currentTheme = resolvedTheme || "light";
+  const currentTheme =
+    resolvedTheme === "dark"
+      ? "dark"
+      : resolvedTheme === "light"
+        ? "light"
+        : "auto";
 
   return (
     <Popover>
       <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
       <PopoverContent className="p-0 w-full border-none shadow-none">
-        <Picker
-          data={data}
+        <EmojiPicker
+          data={data as Data}
           theme={currentTheme}
           onEmojiSelect={(data) => onChange(data.native)}
         />
