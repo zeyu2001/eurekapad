df4b24cf2284d7cb5d00a6a1ebb78d9129674097
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-09-08 20:50:09 +0800

    refactor: use stylespecs

--- components/editor/schema.tsx
+++ components/editor/schema.tsx
@@ -1,12 +1,17 @@
-import { BlockNoteSchema, defaultBlockSpecs } from '@blocknote/core'
+import { BlockNoteSchema, defaultBlockSpecs, defaultStyleSpecs } from '@blocknote/core'
 
 import { codeBlockSpec } from '@/components/editor/code'
 import { mathBlockSpec } from '@/components/editor/math'
 import { transcriptionBlockSpec } from '@/components/editor/transcription'
 
 import { graphBlockSpec } from './graph'
+import { Link } from './styles/link'
 
 export const customSchema = BlockNoteSchema.create({
+  styleSpecs: {
+    ...defaultStyleSpecs,
+    link: Link,
+  },
   blockSpecs: {
     ...defaultBlockSpecs,
     math: mathBlockSpec,
