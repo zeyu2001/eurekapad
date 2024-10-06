c1f354a47a9a3cca79f851961eb765db926f0e95
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-10-06 21:00:31 +0100

    feat: new hero images

--- apps/eurekapad/app/(main)/(routes)/documents/page.tsx
+++ apps/eurekapad/app/(main)/(routes)/documents/page.tsx
@@ -9,6 +9,8 @@ import { toast } from 'sonner'
 
 import { Button } from '@/components/ui/button'
 import { api } from '@/convex/_generated/api'
+import homeImage from '@/images/two-humans.svg'
+import homeImageDark from '@/images/two-humans-dark.svg'
 
 const DocumentsPage = () => {
   const router = useRouter()
@@ -26,9 +28,9 @@ const DocumentsPage = () => {
   }
 
   return (
-    <div className="h-full flex flex-col items-center justify-center space-y-4">
-      <Image src="/empty.png" height="300" width="300" alt="Empty" className="dark:hidden" />
-      <Image src="/empty-dark.png" height="300" width="300" alt="Empty" className="hidden dark:block" />
+    <div className="h-full flex flex-col items-center justify-center space-y-6">
+      <Image src={homeImage} width="350" alt="Empty" className="dark:hidden" />
+      <Image src={homeImageDark} width="350" alt="Empty" className="hidden dark:block" />
       <h2 className="text-lg font-medium">Welcome to {user?.firstName}&apos;s EurekaPad</h2>
       <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
