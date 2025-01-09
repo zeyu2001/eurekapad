00ad21092becfa312b48e39f061f69186528bf80
TheMythologist <leekaixuan2001@gmail.com>
2025-01-09 13:15:06 +0900

    refactor: initialize language runners only when necessary (#48)

--- apps/eurekapad/components/editor/code/code-block.tsx
+++ apps/eurekapad/components/editor/code/code-block.tsx
@@ -157,8 +157,8 @@ export const CodeBlock: FC<ReactCustomBlockRenderProps<CodeBlockConfig, InlineCo
     [getUploadUrl, editorContext],
   )
 
-  const { runner: pythonRunner, loaded: pythonLoaded } = usePythonRunner()
-  const { runner: jsRunner, loaded: jsLoaded } = useJSRunner()
+  const { runner: pythonRunner, loaded: pythonLoaded } = usePythonRunner(language)
+  const { runner: jsRunner, loaded: jsLoaded } = useJSRunner(language)
 
   const handleInputChange = useCallback(
     ({ code, language, height }: { code?: string; language?: string; height?: number }) => {
--- apps/eurekapad/hooks/use-js-runner.tsx
+++ apps/eurekapad/hooks/use-js-runner.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from 'react'
 
 import { SingletonJSRunner } from '@/lib/singleton-js-runner'
 
-export const useJSRunner = () => {
+export const useJSRunner = (language: string) => {
   const [runner, setRunner] = useState<SingletonJSRunner | null>(null)
   const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
+    if (language !== 'javascript' && language !== 'typescript') return
     const runner = SingletonJSRunner.getInstance()
 
     setRunner(runner)
