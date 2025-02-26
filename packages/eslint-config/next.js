7d5bdd790f024d5f71c4b1e53ed44df1bf0d5d24
TheMythologist <leekaixuan2001@gmail.com>
2025-02-27 00:10:53 +0800

    dev: fix eslint on vscode

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -14,7 +14,6 @@ export const nextJsConfig = [
       ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
         ...globals.serviceworker,
-        process: true,
       },
     },
   },
