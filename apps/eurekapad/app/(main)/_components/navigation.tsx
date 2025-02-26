05bad7c467991679830aca49a6b1f3d5ed62d7ad
TheMythologist <leekaixuan2001@gmail.com>
2025-02-27 00:23:58 +0800

    ui: show block options in front of navbar

--- apps/eurekapad/app/(main)/_components/navigation.tsx
+++ apps/eurekapad/app/(main)/_components/navigation.tsx
@@ -120,7 +120,7 @@ export const Navigation = () => {
       <aside
         ref={sidebarRef}
         className={cn(
-          'group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]',
+          'group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-10',
           isResetting && 'transition-all ease-in-out duration-300',
           isMobile && 'w-0',
         )}
