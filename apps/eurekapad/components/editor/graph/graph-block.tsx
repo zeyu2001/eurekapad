ecf3706b6d3933a63721e31b01b2341f648ad973
TheMythologist <leekaixuan2001@gmail.com>
2025-02-27 15:02:11 +0800

    fix: remove ugly desmos workarounds

--- apps/eurekapad/components/editor/graph/graph-block.tsx
+++ apps/eurekapad/components/editor/graph/graph-block.tsx
@@ -1,55 +1,37 @@
-import 'desmos'
-
 import { InlineContentSchema, StyleSchema } from '@blocknote/core'
 import { ReactCustomBlockRenderProps } from '@blocknote/react'
-import { FC, useEffect, useRef, useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { FC, useEffect, useRef } from 'react'
 
 import { GraphBlockConfig } from './config'
-import { graphStateJSONSchema } from './schemas'
-import { GraphState } from './types'
 
 export const GraphBlock: FC<ReactCustomBlockRenderProps<GraphBlockConfig, InlineContentSchema, StyleSchema>> = ({
   block,
   editor,
 }) => {
-  const result = graphStateJSONSchema.safeParse(block.props.state)
-
+  const pathname = usePathname()
   const graphRef = useRef<HTMLDivElement>(null)
-  const [graphState, setGraphState] = useState<GraphState | null>(result.error ? null : result.data)
 
-  const updateEditor = () => {
-    editor.updateBlock(block.id, {
-      props: {
-        ...block.props,
-        state: JSON.stringify(graphState),
-      },
-    })
-  }
-
-  // can only initialize GraphingCalculator after graphRef is mounted
   useEffect(() => {
     if (!graphRef.current) return
 
-    const calculator = Desmos.GraphingCalculator(graphRef.current)
-
-    if (graphState) calculator.setState(graphState)
-
-    calculator.observeEvent('change', () => {
-      setGraphState(calculator.getState() as GraphState)
+    const calculator = Desmos.GraphingCalculator(graphRef.current, {
+      keypad: !pathname?.startsWith('/preview'),
     })
 
-    // on subsequent callbacks, a duplicate graph is appended to the graphRef
-    // this is a workaround to remove the duplicate graphs
-    while (graphRef.current.children.length > 1) graphRef.current.removeChild(graphRef.current.lastChild as ChildNode)
-  }, [graphRef, graphState])
+    // @ts-expect-error desmos is typed wrongly here
+    calculator.observeEvent('change', ({ isUserInitiated }: { isUserInitiated: boolean }) => {
+      if (isUserInitiated)
+        editor.updateBlock(block.id, {
+          props: {
+            ...block.props,
+            state: JSON.stringify(calculator.getState()),
+          },
+        })
+    })
 
-  return (
-    <div
-      ref={graphRef}
-      // update when Desmos input loses focus,
-      // otherwise interactive element will lose focus while user is still typing
-      onBlur={updateEditor}
-      className="w-full h-96 flex items-center justify-center"
-    ></div>
-  )
+    return () => calculator.destroy()
+  }, [block.id, block.props, editor, pathname])
+
+  return <div ref={graphRef} className="flex h-96 w-full items-center justify-center"></div>
 }
