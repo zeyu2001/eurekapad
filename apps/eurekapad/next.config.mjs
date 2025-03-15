4343c0f8d92270fc53456a1a1c4a06e82c455e54
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2025-03-15 16:33:06 +0000

    set up nextjs rewrites

--- apps/eurekapad/next.config.mjs
+++ apps/eurekapad/next.config.mjs
@@ -43,6 +43,27 @@ const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
+
+  // This is required to support PostHog reverse proxy
+  // https://posthog.com/docs/advanced/proxy/nextjs
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://us.i.posthog.com/decide',
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 }
 
 const withMDX = createMDX({
